c9b8113df1959b69175a36c1d95d5fb58d9111f9
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-10-09 15:27:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,77 @@
-const samyak = 'https://api.github.com/users/'
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-async function getUser(username){
-    try{
-        const{ data } = await axios(samyak + username)
-        createUserCard(data)
-        getRepos(username)
-    }
-    catch(err){
-        if(err.response.status == 404){
-            createErrorCard('No profile with this UserName')
-        }
-    }
-}
-async function getRepos(username){
-    try{
-        const{ data }=await axios(samyak + username + '/repos?sort=created')
+const APIURL = "https://api.github.com/users/";
 
-        addReposToCard(data)
-    }
-    catch(err){
-        createErrorCard('Problem Fetching Repos')
-    }
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getUser("Samyak-Bumb");
+
+async function getUser(username) {
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+
+    getRepos(username);
 }
-function createUserCard(user){
-    const userID=user.name || user.login
-    const userBio=user.bio ? `<p>${user.bio}</p>` : ''
-    const cardHTML=
-    `<div class="card">
-    <div>
-      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
-    </div>
-    <div class="user-info">
-      <h2>${userID}</h2>
-      ${userBio}
-      <ul>
-        <li>${user.followers} <strong>Followers</strong></li>
-        <li>${user.following} <strong>Following</strong></li>
-        <li>${user.public_repos} <strong>Repos</strong></li>
-      </ul>
-      <div id="repos"></div>
-    </div>
-  </div>`
-    main.innerHTML=cardHTML
+
+async function getRepos(username) {
+    const resp = await fetch(APIURL + username + "/repos");
+    const respData = await resp.json();
+
+    addReposToCard(respData);
 }
-function createErrorCard(msg){
-    const cardHTML=`<div class="card"><h1>${msg}</h1></div>`
-    main.innerHTML=cardHTML
+
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
 }
-function addReposToCard(repos){
-    const reposE=document.getElementById('repos')
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById("repos");
+
     repos
-        .slice(0, 5) .forEach(repo=>{
-            const repoEl = document.createElement('a')
-            repoEl.classList.add('repo')
-            repoEl.href = repo.html_url
-            repoEl.target = '_blank'
-            repoEl.innerText = repo.name
-            reposEl.appendChild(repoEl)
-        })
+        .sort((a, b) => b.stargazers_count - a.stargazers_count)
+        .slice(0, 10)
+        .forEach((repo) => {
+            const repoEl = document.createElement("a");
+            repoEl.classList.add("repo");
+
+            repoEl.href = repo.html_url;
+            repoEl.target = "_blank";
+            repoEl.innerText = repo.name;
+
+            reposEl.appendChild(repoEl);
+        });
 }
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const user = search.value
-    if(user){
-        getUser(user)
-        search.value = ''
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = "";
     }
-})
+});
